66e1ee7177c8ee2b18b3d1fb8819010b90555430
redphaser <eabullard3@gmail.com>
2013-04-17 08:03:43 +0000

    Changes to logic / order of operations
    Reworked to remove re dependency

--- autosnap.py
+++ autosnap.py
@@ -4,11 +4,11 @@
 This script provides a way to run rotating snapshots of a Rackspace
 Cloud Server on the OpenCloud infrastructure.
 
-Usage: autosnap.py <username> <APIKey> <ServerUUID> <Retention Count> <Region>
+Usage:
+autosnap.py <username> <APIKey> <ServerUUID> <Minimum Retention> <Region>
 """
 
 import pyrax
-import re
 import argparse
 
 
@@ -31,9 +31,9 @@ def main():
     parser.add_argument('key', metavar='apikey', type=str,
                         help='API key for account')
     parser.add_argument('server', metavar='server', type=str,
-                        help='UUID of server to snapshot')
+                        help='UUID of server to autosnap')
     parser.add_argument('retention', metavar='retention', type=int,
-                        help='Number of days to retain snapthots')
+                        help='Minimum number of autosnaps to retain')
     parser.add_argument('region', metavar='region', type=str,
                         help='Region of Server (DFW, ORD, LON)',
                         choices=['DFW', 'ORD', 'LON'])
@@ -42,26 +42,29 @@ def main():
 
     pyrax.set_credentials(args.user, args.key)
 
+    #Set up a cloud object based on region specified by user
     cloud = pyrax.connect_to_cloudservers(region=args.region)
+
     server = cloud.servers.get(args.server)
     images = cloud.images.list()
 
     snap_list = []
+
     snap_name = server.name + "-autosnap-" + args.server
 
     #Search for previous autosnaps of server and append to a list
     for i in images:
-        search_name = "-autosnap-" + args.server
-        if re.search(search_name, i.name + "$"):
+        search_name = server.name + "-autosnap-" + server.id
+        if i.name == search_name:
             snap_list.append(i)
-
-    #Create snapshot if retention GTE number of previous autosnaps
-    if args.retention >= len(snap_list):
-        server.create_image(snap_name)
 
     #Delete oldest snapshot(s) if retention < num images
     while args.retention < len(snap_list):
         snap_list = delete_old(snap_list, cloud)
+
+    #Create snapshot if retention >= number of previous autosnaps
+    if args.retention >= len(snap_list):
+        server.create_image(snap_name)
 
 
 if __name__ == '__main__':
